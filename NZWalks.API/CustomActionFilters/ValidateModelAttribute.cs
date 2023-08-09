9fa60eaebc9b38f100543967941c74820dff23ed
udaymathur <udaymathur14@gmail.com>
2023-08-09 15:44:19 +0530

    Correct Comments

--- NZWalks.API/CustomActionFilters/ValidateModelAttribute.cs
+++ NZWalks.API/CustomActionFilters/ValidateModelAttribute.cs
@@ -5,7 +5,7 @@ namespace NZWalks.API.CustomActionFilters
 {
     public class ValidateModelAttribute : ActionFilterAttribute
     {
-        //yeh check krega ki jo bhi hmne attribute daale h dto ke ander like required field and aage bhi unsbko validate 
+        //yeh check krega ki jo bhi hmne attribute or fields daali h dto ke upar like required field and aage bhi unsbko validate 
         //krne ke liye baar baar modelisvalid use krna pdta h controller ke ander toh wo kaafi bda ho jaata h toh isliye 
         // usko chota krne ke liye hme yeh use kra jo ki hmara kaam aasan bna dega 
 
