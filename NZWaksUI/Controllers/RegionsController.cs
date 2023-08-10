681d64aa7f0579b0fa45fad90b83b6b4aad954de
udaymathur <udaymathur14@gmail.com>
2023-08-10 18:10:14 +0530

    View Regions in Web

---
+++ NZWaksUI/Controllers/RegionsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWaksUI.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var response = await client.GetAsync("https://localhost:7209/api/regions");
+
+                response.EnsureSuccessStatusCode();
+
+                var stringResponseBody = await response.Content.ReadAsStringAsync();
+
+                ViewBag.response = stringResponseBody;
+            }
+            catch(Exception ex) 
+            {
+
+            }
+
+            return View();
+        }
+    }
+}
